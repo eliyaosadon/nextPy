a7a7cb6c8320b1f0501bd10999d7ff4253226be1
eliyaosadon <eliaosadon@gmail.com>
2024-05-26 20:26:58 +0300

    first mission

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from unit_1 import find_longest_name, sum_of_name_lengths, print_shortest_names, create_name_length_file
 
-from unit_1 import find_longest_name
 
 def main():
     filename = 'names.txt'
-    longest_name = find_longest_name(filename)
-    print(longest_name)
-
+    #    longest_name = find_longest_name(filename)
+    #    print(longest_name)
+    #    sum_of_name_lengths(filename)
+    # קריאה לפונקציה עם שם הקובץ 'names.txt'
+    # print_shortest_names('names.txt')
+    input_filename = 'names.txt'
+    output_filename = 'name_length.txt'
+    create_name_length_file(input_filename, output_filename)
 if __name__ == "__main__":
     main()
--- unit_1.py
+++ unit_1.py
@@ -1,3 +1,19 @@
 #
 def find_longest_name(filename):
     with open(filename) as f: return max(f.read().splitlines(), key=len)
+
+
+def sum_of_name_lengths(filename):
+    with open(filename) as f:
+        print(sum(len(name) for name in f.read().splitlines()))
+
+
+def print_shortest_names(filename):
+    with open(filename) as f: names = f.read().splitlines()
+    min_length = min(map(len, names))
+    print('\n'.join(name for name in names if len(name) == min_length))
+
+
+def create_name_length_file(input_filename, output_filename):
+    with open(input_filename) as f_in, open(output_filename, 'w') as f_out:
+        f_out.write('\n'.join(str(len(name.strip())) for name in f_in))
