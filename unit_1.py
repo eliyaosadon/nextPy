87d09405771fb2c05e7bfe36aabf8c125ed55a3a
eliyaosadon <eliaosadon@gmail.com>
2024-05-27 00:24:31 +0300

    first mission

--- unit_1.py
+++ unit_1.py
@@ -1,24 +1,28 @@
-#
+# Ex.1 - finds the longest name in the file
 def find_longest_name(filename):
     with open(filename) as f: return max(f.read().splitlines(), key=len)
 
 
+# Ex.2 - prints the sum of all the names in the file
 def sum_of_name_lengths(filename):
     with open(filename) as f:
         print(sum(len(name) for name in f.read().splitlines()))
 
 
+# Ex.3 - prints the shortest names in the file
 def print_shortest_names(filename):
     with open(filename) as f: names = f.read().splitlines()
     min_length = min(map(len, names))
     print('\n'.join(name for name in names if len(name) == min_length))
 
 
+# Ex.4 - creates new file that includes all the names lengths in the original file
 def create_name_length_file(input_filename, output_filename):
     with open(input_filename) as f_in, open(output_filename, 'w') as f_out:
         f_out.write('\n'.join(str(len(name.strip())) for name in f_in))
 
 
+# Ex.5 - asks for a number and presents all the names that their length equals to the input number
 def print_names_by_length(length, filename):
     with open(filename) as f:
         names = [name.strip() for name in f if len(name.strip()) == length]
---
+++ unit_2/Animal.py
@@ -0,0 +1,124 @@
+class Animal:
+    zoo_name = "Hayaton"
+
+    def __init__(self, name, hunger=0):
+        self.name_ = name
+        self.hunger_ = hunger
+
+    def get_name(self):
+        return self.name_
+
+    def is_hungry(self):
+        return self.hunger_ > 0
+
+    def feed(self):
+        if self.is_hungry():
+            self.hunger_ -= 1
+
+    def __str__(self):
+        return 'Animal'
+
+    def talk(self):
+        pass
+
+
+class Dog(Animal):
+    def __str__(self):
+        return 'Dog'
+
+    def talk(self):
+        print('woof woof')
+
+    def fetch_stick(self):
+        print('There you go, sir!')
+
+
+class Cat(Animal):
+    def __str__(self):
+        return 'Cat'
+
+    def talk(self):
+        print('meow')
+
+    def chase_laser(self):
+        print('Meeeeow')
+
+
+class Skunk(Animal):
+    def __init__(self, name, hunger=0):
+        self._stink_count = 6
+        super().__init__(name, hunger)
+
+    def __str__(self):
+        return 'Skunk'
+
+    def talk(self):
+        print('tsssss')
+
+    def stink(self):
+        print('Dear lord!')
+
+
+class Unicorn(Animal):
+
+    def __str__(self):
+        return 'Unicorn'
+
+    def talk(self):
+        print('Good day, darling')
+
+    def sing(self):
+        print('I’m not your toy...')
+
+
+class Dragon(Animal):
+
+    def __init__(self, name, hunger=0):
+        self._color = 'Green'
+        super().__init__(name, hunger)
+
+    def __str__(self):
+        return 'Dragon'
+
+    def talk(self):
+        print('Raaaawr')
+
+    def breath_fire(self):
+        print('$@#$#@$')
+
+
+def main():
+    zoo_lst = [
+        Dog("Brownie", 10),
+        Cat("Zelda", 3),
+        Skunk("Stinky"),
+        Unicorn("Keith", 7),
+        Dragon("Lizzy", 1450),
+        Dog("Doggo", 80),
+        Cat("Kitty", 80),
+        Skunk("Stinky Jr.", 80),
+        Unicorn("Clair", 80),
+        Dragon("McFly", 80)
+    ]
+
+    for animal in zoo_lst:
+        print(f'{animal} {animal.get_name()}')
+        while animal.is_hungry():
+            animal.feed()
+        animal.talk()
+        if isinstance(animal, Dog):
+            animal.fetch_stick()
+        elif isinstance(animal, Cat):
+            animal.chase_laser()
+        elif isinstance(animal, Skunk):
+            animal.stink()
+        elif isinstance(animal, Unicorn):
+            animal.sing()
+        elif isinstance(animal, Dragon):
+            animal.breath_fire()
+
+    print(Animal.zoo_name)
+
+
+if __name__ == "__main__":
+    main()
